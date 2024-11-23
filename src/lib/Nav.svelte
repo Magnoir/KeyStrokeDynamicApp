1de381f1594e8d759aa663ab3574468bbe3327d6
Gustave Legrand <legrandgustave2001@gmail.com>
2024-11-23 14:10:57 +0100

    Improve navigability

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,15 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-primary bg-primary ">
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-                <a class="nav-link text-white" href="/home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="/database">Database</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/database">Database</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
